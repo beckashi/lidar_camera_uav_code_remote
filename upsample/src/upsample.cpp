b1a19d07a78f9c8b89597175114b01e1e44c8747
jplhbird <120156842@qq.com>
2021-08-08 22:27:45 +0800

    ini

---
+++ upsample/src/upsample.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <chrono>
+
+using namespace std;
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+int upsample(int a, int b) {
+  return 0;
+}
---
+++ upsample/src/upsample.h
@@ -0,0 +1,7 @@
+#ifndef UPSAMPLE_H_
+#define UPSAMPLE_H_
+
+int upsample(int a, int b);
+
+#endif 
+
