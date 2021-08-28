04f660c1246660b126993f7b7c71084a4376d288
beckashi <2535009447@qq.com>
2021-08-28 17:35:54 +0800

    828

---
+++ upsample/bulid/CMakeFiles/imageBasics.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/becka/vstest/lidar_camera_uav_code_remote/upsample/src/imageBasics.cpp" "/home/becka/vstest/lidar_camera_uav_code_remote/upsample/bulid/CMakeFiles/imageBasics.dir/src/imageBasics.cpp.o"
+  "/home/becka/vstest/lidar_camera_uav_code_remote/upsample/src/upsample.cpp" "/home/becka/vstest/lidar_camera_uav_code_remote/upsample/bulid/CMakeFiles/imageBasics.dir/src/upsample.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
